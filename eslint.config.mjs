0f286fc341e4d8d4b5c21b611aa80c5d36bfbe6a
Mati 🔥 <msamsel@users.noreply.github.com>
2025-05-02 16:16:29 +0200

    Fix config, maybe

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,22 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import importPlugin from "eslint-plugin-import";
+import { globalIgnores } from "eslint/config";
 
 export default tseslint.config(
+  globalIgnores(["*.config.*.m?(j|t)s"]),
   eslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
   {
     rules: {
       "import/extensions": [
