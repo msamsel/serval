fcc68a2f51e957cd051ed7c7eaa8009e35f8611a
Mati 🔥 <msamsel@users.noreply.github.com>
2025-05-18 23:16:53 +0200

    Add building steps and improve process

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,26 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import importPlugin from "eslint-plugin-import";
-import { globalIgnores } from "eslint/config";
 
 export default tseslint.config(
-  globalIgnores(["examples/**/*.*", "eslint.config.mjs", "dist/**/*.*"]),
   eslint.configs.recommended,
   importPlugin.flatConfigs.recommended,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
   {
+    files: ["src/**/*.ts"],
     languageOptions: {
       parserOptions: {
-        project: true,
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: [/\.config\.(ts|js|mjs)$/],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.dev.json",
         tsconfigRootDir: import.meta.dirname,
       },
     },
